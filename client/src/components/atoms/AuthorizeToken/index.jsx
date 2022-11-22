78842192fdcdc895747935214e6976d609e89371
jhoacar <jhoacar.utn@gmail.com>
2022-11-22 20:48:08 -0300

    added new styles in unauthorized component

--- client/src/components/atoms/AuthorizeToken/index.jsx
+++ client/src/components/atoms/AuthorizeToken/index.jsx
@@ -1,12 +1,16 @@
 import { getToken } from '../../../utils/token';
-
+import { Card, CardContent, Typography } from '@mui/material';
 export default function AuthorizeToken({ children }) {
   const token = getToken();
   if (!token) {
     return (
-      <div>
-        Ud no esta autorizado
-      </div>
+      <Card>
+        <CardContent sx={{textAlign: 'center'}}>
+          <Typography variant="h5">
+            Ud no esta autorizado
+          </Typography>
+        </CardContent>
+      </Card>
     );
   }
 
